e53eeeb1a0e8861b7bc040319dabf7416e3992ed
Rosalino_Cabral <rosalino.cabral@gmail.com>
2022-11-28 16:19:33 -0300

    Creación del método guardarFormulario
    Resolver problema de url

--- pagina/forms.py
+++ pagina/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from .models import Formulario
 
+
 class FormularioForm(forms.ModelForm):
     class Meta:
         model = Formulario
-        fields = '__all__'
+        fields = ['nombre',
+                  'empresa',
+                  'email',
+                  'telefono',
+                  'ciudad',
+                  'soporte_actual',
+                  'facturacion_mensual',
+                  'descripcion', ]
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'Placeholder': 'Nombre'}),
+            'empresa': forms.TextInput(attrs={'Placeholder': 'Empresa'}),
+            'email': forms.EmailInput(attrs={'Placeholder': 'Correo Electronico'}),
+            'telefono': forms.TextInput(attrs={'Placeholder': 'Celular'}),
+            'ciudad': forms.TextInput(attrs={'Placeholder': 'Ciudad'}),
+            'soporte_actual': forms.TextInput(attrs={'Placeholder': 'Soporte Actual'}),
+            'facturacion_mensual': forms.TextInput(attrs={'Placeholder': 'Facturación Mensual'}),
+            'descripcion': forms.Textarea(attrs={'Placeholder': 'Describenos para conocerte mejor'}), }
--- pagina/views.py
+++ pagina/views.py
@@ -1,10 +1,7 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-
-
-
-# Create your views here.
-from pagina.forms import FormularioForm
+from .models import Formulario
+from .forms import FormularioForm
 
 
 def vistaIndex(request):
@@ -19,12 +16,36 @@ def portafolio(request):
 def nuestro_servicio(request):
     return render(request, 'paginas/nuestro_servicio.html')
 
+
 def insertarFormulario(request):
-    formulario = FormularioForm(request.POST or None)
-    if formulario.is_valid():
-        formulario.save()
-        return render(request, 'paginas/registrarFormulario.html', {'formulario': formulario})
-    return render(request, 'paginas/index.html')
+    contex = {}
+    template_name = 'registrarFormulario.html'
+    if request.method == "POST":
+        form = FormularioForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get('nombre')
+            empresa = form.cleaned_data.get('empresa')
+            email = form.cleaned_data.get('email')
+            telefono = form.cleaned_data.get('telefono')
+            ciudad = form.cleaned_data.get('ciudad')
+            soporte_actual = form.cleaned_data.get('soporte_actual')
+            facturacion_mensual = form.cleaned_data.get('facturacion_mensual')
+            descripcion = form.cleaned_data.get('descripcion')
+            registrar = Formulario.objects.create(
+                nombre=nombre,
+                empresa=empresa,
+                email=email,
+                telefono=telefono,
+                ciudad=ciudad,
+                soporte_actual=soporte_actual,
+                facturacion_mensual=facturacion_mensual,
+                descripcion=descripcion
+            )
+            registrar.save()
+    else:
+        form = FormularioForm()
+    contex['form'] = form
+    return render(request, template_name, contex)
 
 
 def producto(request):
